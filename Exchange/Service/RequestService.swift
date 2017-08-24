217dd5efe7b5dc7f6b2452eece7bbe1f8038eb2e
Quang Vu <qdv2130@rit.edu>
2017-08-23 23:42:13 -0400

    Refactor deleting request code

--- Exchange/Service/RequestService.swift
+++ Exchange/Service/RequestService.swift
@@ -68,6 +68,7 @@ class RequestService {
                 dispatchGroup.enter()
                 Database.database().reference().child("Requests/\(requestRef)").observeSingleEvent(of: .value, with: { snapshot in
                     guard let request = Request(snapshot: snapshot) else {
+                        dispatchGroup.leave()
                         return
                     }
                     requestList.append(request)
@@ -101,12 +102,14 @@ class RequestService {
                 dispatchGroup.enter()
                 Database.database().reference().child("Requests/\(requestRef)").observeSingleEvent(of: .value, with: { snapshot in
                     guard let request = Request(snapshot: snapshot) else {
+                        dispatchGroup.leave()
                         return
                     }
                     requestList.append(request)
                     dispatchGroup.leave()
                 })
             }
+            
             dispatchGroup.notify(queue: .main, execute: {
                 completionHandler(requestList)
             })
@@ -118,28 +121,67 @@ class RequestService {
      from the database
     */
     static func deleteRequest(from requestRef: DatabaseReference, requestID: String, completionHandler: @escaping (Bool) -> Void) {
+
         requestRef.removeValue(completionBlock: { (error, _) in
             if let error = error {
                 assertionFailure("Failed to remove request reference from user: " + error.localizedDescription)
                 return completionHandler(false)
             }
-        })
-        // Update the participant number of the request
-        let participantRef = Database.database().reference().child("requests/\(requestID)/participant_num")
-        participantRef.runTransactionBlock({ (mutableData) -> TransactionResult in
-            let currNum = mutableData.value as! Int
-            mutableData.value = currNum - 1
-            return TransactionResult.success(withValue: mutableData)
-        }, andCompletionBlock: { (error, _, _) in
-            if let error = error {
-                assertionFailure("Failed to update request's participant number: " + error.localizedDescription)
-                return completionHandler(false)
-            } else {
-                return completionHandler(true)
-            }
+            
+            // Update the participant number of the request
+            let participantRef = Database.database().reference().child("Requests/\(requestID)/participant_num")
+            participantRef.runTransactionBlock({ (mutableData) -> TransactionResult in
+                let currNum = mutableData.value as? Int ?? 2
+                mutableData.value = currNum - 1
+                return TransactionResult.success(withValue: mutableData)
+            }, andCompletionBlock: { (error, _, snapshot) in
+                if let error = error {
+                    assertionFailure("Failed to update request's participant number: " + error.localizedDescription)
+                    return completionHandler(false)
+                } else {
+                    // Delete the entire request if there is no participant
+                    if let participantNum = snapshot?.value as? Int {
+                        if participantNum == 0 {
+                            let ref = Database.database().reference().child("Requests/\(requestID)")
+                            ref.setValue(nil)
+                        }
+                    }
+                    return completionHandler(true)
+                }
+            })
         })
     }
-
+    
+    /**
+     Update a request -> either cancel or reject a request. Update the status of the request, and
+     the requester list of each requested items
+    */
+    static func updateRequest(for request: Request, completion: @escaping (Bool) -> Void) {
+        guard let requestKey = request.requestKey else {
+            return completion(false)
+        }
+        var data: [String: Any] = [
+            "Requests/\(requestKey)/status": "Cancelled"
+        ]
+        let posterItemsKey = Array(request.posterItemsData.keys)
+        for itemRef in posterItemsKey {
+            data["allItems/\(itemRef)/requested_by/\(request.requesterID)"] = [:]
+        }
+        
+        let requesterItemsKey = Array(request.requesterItemsData.keys)
+        for itemRef in requesterItemsKey {
+            data["allItems/\(itemRef)/requested_by/\(request.requesterID)"] = [:]
+        }
+        
+        Database.database().reference().updateChildValues(data, withCompletionBlock: { (error, _) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return completion(false)
+            }
+            
+            return completion(true)
+        })
+    }
 }
 
 
--- Exchange/View Controllers/RequestDetailViewController.swift
+++ Exchange/View Controllers/RequestDetailViewController.swift
@@ -88,45 +88,16 @@ class RequestDetailViewController: UIViewController {
     @IBAction func rejectRequest(_ sender: UIButton) {
         let alertController = UIAlertController(title: nil, message: "Are you sure?", preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "No", style: .cancel, handler: nil)
-        let confirmAction = UIAlertAction(title: "Yes", style: .default, handler: { (action) in
-            // Update the request status on the database + all items' availability + remove user from the requested by field
-            guard let request = self.request,
-                let requestRef = request.requestKey else {
-                    return
+        let confirmAction = UIAlertAction(title: "Yes", style: .default, handler: { [unowned self] (action) in
+            guard let request = self.request else {
+                return
             }
             
-            var data: [String: Any] = [
-                "Requests/\(requestRef)/status": "Rejected"
-            ]
-            let posterItemsKey = Array(request.posterItemsData.keys)
-            for itemRef in posterItemsKey {
-                data["allItems/\(itemRef)/requested_by/\(request.requesterID)"] = [:]
-            }
-            
-            let requesterItemsKey = Array(request.requesterItemsData.keys)
-            for itemRef in requesterItemsKey {
-                data["allItems/\(itemRef)/requested_by/\(request.requesterID)"] = [:]
-            }
-            
-            // While this is going on, we can also display a spinner
-            let dispatchGroup = DispatchGroup()
-            var success = true
-            dispatchGroup.enter()
-            Database.database().reference().updateChildValues(data, withCompletionBlock: { (error, _) in
-                if let error = error {
-                    assertionFailure(error.localizedDescription)
-                    success = false
-                    dispatchGroup.leave()
-                    return
-                }
-                dispatchGroup.leave()
-            })
-            
-            dispatchGroup.notify(queue: .main, execute: {
+            RequestService.updateRequest(for: request, completion: { (success) in
                 if success {
                     let alertController
                         = UIAlertController(title: nil, message: "Request Rejected!", preferredStyle: .alert)
-                    let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: { (action) in 
+                    let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: { (action) in
                         self.performSegue(withIdentifier: "Back", sender: nil)
                     })
                     alertController.addAction(cancelAction)
@@ -140,49 +111,22 @@ class RequestDetailViewController: UIViewController {
                 }
             })
         })
+        
         alertController.addAction(cancelAction)
         alertController.addAction(confirmAction)
         self.present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func cancelRequest(_ sender: UIButton) {
-        let alertController = UIAlertController(title: nil, message: "Are you sure?", preferredStyle: .alert)
+        let alertController = UIAlertController(title: nil, message: "You are about to cancel this request.", preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "No", style: .cancel, handler: nil)
-        let confirmAction = UIAlertAction(title: "Yes", style: .default, handler: { (action) in
-            // Update the request status on the database + all items' availability + remove user from the requested by field
-            guard let request = self.request,
-                let requestRef = request.requestKey else {
-                    return
+        let confirmAction = UIAlertAction(title: "Yes", style: .default, handler: { [unowned self] (action) in
+            guard let request = self.request else {
+                return
             }
             
-            var data: [String: Any] = [
-                "Requests/\(requestRef)/status": "Cancelled"
-            ]
-            let posterItemsKey = Array(request.posterItemsData.keys)
-            for itemRef in posterItemsKey {
-                data["allItems/\(itemRef)/requested_by/\(request.requesterID)"] = [:]
-            }
-            
-            let requesterItemsKey = Array(request.requesterItemsData.keys)
-            for itemRef in requesterItemsKey {
-                data["allItems/\(itemRef)/requested_by/\(request.requesterID)"] = [:]
-            }
-            
-            // While this is going on, we can also display a spinner
-            let dispatchGroup = DispatchGroup()
-            var success = true
-            dispatchGroup.enter()
-            Database.database().reference().updateChildValues(data, withCompletionBlock: { (error, _) in
-                if let error = error {
-                    assertionFailure(error.localizedDescription)
-                    success = false
-                    dispatchGroup.leave()
-                    return
-                }
-                dispatchGroup.leave()
-            })
-            
-            dispatchGroup.notify(queue: .main, execute: {
+            // Display spinner
+            RequestService.updateRequest(for: request, completion: { (success) in
                 if success {
                     let alertController
                         = UIAlertController(title: nil, message: "Request Cancelled!", preferredStyle: .alert)
@@ -206,8 +150,61 @@ class RequestDetailViewController: UIViewController {
     }
     
     @IBAction func deleteRequest(_ sender: Any) {
-        // Delete the current request from the database
+        let confirmDeleteController = UIAlertController.init(title: nil, message: "Are you sure that you want to delete this request?", preferredStyle: .alert)
+        let cancelAction = UIAlertAction.init(title: "No", style: .cancel, handler: nil)
+        let confirmAction = UIAlertAction.init(title: "Yes", style: .default, handler: { [unowned self] (action) in
+            UIApplication.shared.beginIgnoringInteractionEvents()
+            let dispGroup = DispatchGroup()
+            var requestRef: DatabaseReference
+            
+            // Unwrap request's key
+            guard let request = self.request,
+                let requestKey =  request.requestKey else {
+                UIApplication.shared.endIgnoringInteractionEvents()
+                return
+            }
+            
+            switch self.index! {
+            case 0:
+                requestRef = Database.database().reference().child("users/\(User.currentUser.uid)/Outgoing Request")
+            case 1:
+                requestRef = Database.database().reference().child("users/\(User.currentUser.uid)/Incoming Request")
+            default:
+                UIApplication.shared.endIgnoringInteractionEvents()
+                fatalError("Unrecognized index")
+            }
+            
+            dispGroup.enter()
+            RequestService.updateRequest(for: request, completion: { (success) in
+                if !success {
+                    self.displayWarningMessage(message: "Failed to delete this request. Please check your network and try again")
+                    dispGroup.leave()
+                } else {
+                    RequestService.deleteRequest(from: requestRef, requestID: requestKey, completionHandler: { (success) in
+                        if success {
+                            let alertController = UIAlertController.init(title: nil, message: "Request deleted.", preferredStyle: .alert)
+                            let acknowledgeMessageAction = UIAlertAction(title: "OK", style: .cancel, handler: { (action) in
+                                self.performSegue(withIdentifier: "Back", sender: nil)
+                            })
+                            alertController.addAction(acknowledgeMessageAction)
+                            self.present(alertController, animated: true, completion: nil)
+                        } else {
+                            self.displayWarningMessage(message: "Failed to delete this request. Please check your network and try again")
+                        }
+                        dispGroup.leave()
+                    })
+                }
+            })
+            
+            
+            dispGroup.notify(queue: .main, execute: {
+                UIApplication.shared.endIgnoringInteractionEvents()
+            })
+        })
         
+        confirmDeleteController.addAction(confirmAction)
+        confirmDeleteController.addAction(cancelAction)
+        self.present(confirmDeleteController, animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
